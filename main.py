a5c3482968bcc39a98d8983ba7278d63a564bde0
Phonedolly <Phonedolly@users.noreply.github.com>
2022-08-07 22:53:43 +0900

    complete!

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
+from __future__ import print_function
+
+__author__ = "jcgregorio@google.com (Joe Gregorio)"
+
 import json
-from google.auth.transport.requests import AuthorizedSession
-from google_auth_oauthlib.flow import InstalledAppFlow
+import sys
+
+from oauth2client import client
+from googleapiclient import sample_tools
 
 SCOPE = 'https://www.googleapis.com/auth/blogger'
 
 
-def init() -> AuthorizedSession:
-    flow = InstalledAppFlow.from_client_secrets_file('./client_secrets.json', scopes=[SCOPE])
-    flow.run_local_server()
-    return flow.authorized_session()
+def main(argv):
+    # Authenticate and construct service.
+    service, flags = sample_tools.init(
+        argv,
+        "blogger",
+        "v3",
+        __doc__,
+        __file__,
+        scope="https://www.googleapis.com/auth/blogger",
+    )
+
+    try:
+        posts = service.posts()
+
+        print("Input New Post URL: ", end='')
+        new_url = input()
+
+        blog_id = None
+        post_id = None
+
+        with open('settings.json', encoding='utf-8') as f:
+            settings = json.load(f)
+            blog_id = str(settings['blogID'])
+            post_id = str(settings['postID'])
+
+        body = posts.get(blogId=blog_id, postId=post_id).execute()
+        body['content'] = body['content'] \
+                          + '<br><a href="' + new_url + '">' + new_url + '&nbsp;</a>'  # 문자열을 읽을 때에는 json.loads()를 사용한다
+
+        res = posts.update(blogId=blog_id, postId=post_id, body=body).execute()
+        print(res)
+
+    except client.AccessTokenRefreshError:
+        print(
+            "The credentials have been revoked or expired, please re-run"
+            "the application to re-authorize"
+        )
 
 
-def task_post(session: AuthorizedSession) -> None:
-    print("Input New Post URL: ", end='')
-    newURL = input()
-
-    blogID = None
-    postID = None
-
-    with open('settings.json', encoding='utf-8') as f:
-        settings = json.load(f)
-        blogID = str(settings['blogID'])
-        postID = str(settings['postID'])
-
-    res = session.get('https://www.googleapis.com/blogger/v3/blogs/' + blogID + '/posts/' + postID)
-    body = json.loads(res.content.decode('utf-8'))
-    body['content'] = \
-        body['content'] \
-        + '<br><a href="' + newURL + '">' + newURL + '&nbsp;</a>'  # 문자열을 읽을 때에는 json.loads()를 사용한다
-
-    session.put(url='https://www.googleapis.com/blogger/v3/blogs/' + blogID + '/posts/' + postID, data=body)
-
-    print()
-
-
-# <a href="https://hello220807.blogspot.com/2022/08/useful-links.html">https://hello220807.blogspot.com/2022/08/useful-links.html&nbsp;</a>
-
-if __name__ == '__main__':
-    _session = init()
-    task_post(_session)
-    _session.close()
+if __name__ == "__main__":
+    main(sys.argv)
