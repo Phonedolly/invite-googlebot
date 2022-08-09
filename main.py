cee518326ba383e38634719fb2c6bc2106413c4b
Phonedolly <Phonedolly@users.noreply.github.com>
2022-08-09 22:04:11 +0900

    upload with post's title

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from __future__ import print_function
+
 import json
+import sys
+
+import html_to_json
+import requests
+from bs4 import BeautifulSoup
 
 from oauth2client import client
 from googleapiclient import sample_tools
@@ -6,7 +13,7 @@ from googleapiclient import sample_tools
 SCOPE = 'https://www.googleapis.com/auth/blogger'
 
 
-def poster(robotable_url: str, posts):
+def poster(robotable_url: str, title: str, posts):
     try:
         with open('settings.json', encoding='utf-8') as f:
             settings = json.load(f)
@@ -14,7 +21,8 @@ def poster(robotable_url: str, posts):
             post_id = str(settings['postID'])
 
         body = posts.get(blogId=blog_id, postId=post_id).execute()
-        body['content'] = body['content'] + '<br><a href="' + robotable_url + '">' + robotable_url + '</a>'
+        body['content'] = body[
+                              'content'] + '<a href="' + robotable_url + '">' + robotable_url + '</a> ' + title + '<br>'
 
         res = posts.update(blogId=blog_id, postId=post_id, body=body).execute()
         print('업데이트 완료')
@@ -26,13 +34,15 @@ def poster(robotable_url: str, posts):
         )
 
 
-def extract_robotable_link(post_url: str, naver_id: str) -> str:
-    _post_id = post_url[post_url.rfind('/') + 1:]
-    return 'https://blog.naver.com/PostView.naver?blogId=' + naver_id + '&logNo=' \
-           + _post_id + '&redirect=Dlog&widgetTypeCall=true&directAccess=false'
+def extract_robotable_link_and_title(post_url: str, naver_id: str) -> str:
+    robotable_url = 'https://blog.naver.com' + \
+                    BeautifulSoup(requests.get(post_url).content, features='lxml').select_one('iframe')['src']
+    title = BeautifulSoup(requests.get(robotable_url).content, features='lxml').select_one('title').string
+
+    return robotable_url, title
 
 
-def init() -> tuple:
+def init(argv) -> tuple:
     # Authenticate and construct service.
     _service, _flags = sample_tools.init(
         '',
@@ -53,9 +63,14 @@ def init() -> tuple:
     return _naver_id, _blog_id, _post_id, _service
 
 
-if __name__ == "__main__":
-    __naver_id, __blog_id, __post_id, __service = init()
+def test():
+    print()
 
+
+if __name__ == "__main__":
+    test()
+    __naver_id, __blog_id, __post_id, __service = init(sys.argv)
+    __posts = __service.posts()
     print('Q를 입력할 때까지 계속 입력할 수 있습니다')
 
     while True:
@@ -66,4 +81,5 @@ if __name__ == "__main__":
             __service.close()
             break
         else:
-            poster(extract_robotable_link(input_data, naver_id=__naver_id), posts=__service.posts())
+            _link, _title = extract_robotable_link_and_title(input_data, naver_id=__naver_id)
+            poster(_link, _title, posts=__posts)
